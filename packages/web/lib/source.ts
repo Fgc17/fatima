8d0528ec9af49e49529518b0f562b2a8083bdd65
fgc17 <fgcoelho1709@outlook.com>
2025-02-21 08:37:53 -0300

    refactor (web): remove unused deps

--- packages/web/lib/source.ts
+++ packages/web/lib/source.ts
@@ -1,8 +1,6 @@
 import { docs, meta } from "@/.source";
 import { createMDXSource } from "fumadocs-mdx";
 import { loader } from "fumadocs-core/source";
-import * as icons from "@heroicons/react/16/solid";
-import { createElement } from "react";
 import { Icon } from "@/components/icon";
 
 export const source = loader({
