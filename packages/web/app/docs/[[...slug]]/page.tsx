4c5ae924254eb2b30a8e5ba923109e1b24c69942
fgc17 <fgcoelho1709@outlook.com>
2025-02-21 08:07:54 -0300

    docs (web): add V and X mark svg icons for comparison table

--- packages/web/app/docs/[[...slug]]/page.tsx
+++ packages/web/app/docs/[[...slug]]/page.tsx
@@ -10,6 +10,7 @@ import defaultMdxComponents from "fumadocs-ui/mdx";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import { Callout } from "fumadocs-ui/components/callout";
 import { metadataImage } from "@/lib/metadata";
+import { Icon } from "@/components/icon";
 
 export default async function Page(props: {
 	params: Promise<{ slug?: string[] }>;
@@ -20,12 +21,28 @@ export default async function Page(props: {
 
 	const MDX = page.data.body;
 
+	const V = () => (
+		<Icon icon="CheckIcon" className="w-5 h-5 fill-emerald-500" />
+	);
+
+	const X = () => <Icon icon="XMark" className="w-5 h-5 fill-red-500" />;
+
 	return (
 		<DocsPage toc={page.data.toc} full={page.data.full}>
 			<DocsTitle>{page.data.title}</DocsTitle>
 			<DocsDescription>{page.data.description}</DocsDescription>
 			<DocsBody>
-				<MDX components={{ ...defaultMdxComponents, Tab, Tabs, Callout }} />
+				<MDX
+					components={{
+						...defaultMdxComponents,
+						V,
+						X,
+						Icon,
+						Tab,
+						Tabs,
+						Callout,
+					}}
+				/>
 			</DocsBody>
 		</DocsPage>
 	);
