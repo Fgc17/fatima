931f5f44f6e95fca4334285fe5042d534f9d1ad5
fgc17 <fgcoelho1709@outlook.com>
2025-02-21 08:07:31 -0300

    docs (web): add icon component

---
+++ packages/web/components/icon.tsx
@@ -0,0 +1,29 @@
+import { createElement } from "react";
+import * as icons from "@heroicons/react/16/solid";
+
+export function Icon({
+	icon,
+	className,
+}: {
+	icon: string | undefined;
+	className?: string;
+}) {
+	if (!icon) {
+		return;
+	}
+
+	if (!icon.endsWith("Icon")) {
+		icon = `${icon}Icon`;
+	}
+
+	if (icon && !(icon in icons)) {
+		console.log(`Icon not found: ${icon}`);
+
+		return;
+	}
+
+	if (icon in icons)
+		return createElement(icons[icon as keyof typeof icons], {
+			className,
+		});
+}
--- packages/web/lib/source.ts
+++ packages/web/lib/source.ts
@@ -3,27 +3,14 @@ import { createMDXSource } from "fumadocs-mdx";
 import { loader } from "fumadocs-core/source";
 import * as icons from "@heroicons/react/16/solid";
 import { createElement } from "react";
+import { Icon } from "@/components/icon";
 
 export const source = loader({
 	baseUrl: "/docs",
 	source: createMDXSource(docs, meta),
 	icon(iconString) {
-		let icon = iconString;
-
-		if (!icon) {
-			return;
-		}
-
-		if (!icon.endsWith("Icon")) {
-			icon = `${icon}Icon`;
-		}
-
-		if (icon && !(icon in icons)) {
-			console.log(`Icon not found: ${icon}`);
-
-			return;
-		}
-
-		if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+		return Icon({
+			icon: iconString,
+		});
 	},
 });
