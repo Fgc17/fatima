4d5c1da24793889ac8e01c59b03afad2ce27c970
fgc17 <fgcoelho1709@outlook.com>
2025-03-21 03:38:09 -0300

    feat (fatima): expose parse function

--- packages/fatima/src/core/core.ts
+++ packages/fatima/src/core/core.ts
@@ -1,8 +1,9 @@
+import { parseEnvLines as parse } from "lib/env/parse-env";
 import { adapters } from "./adapters";
 import { linter } from "./linter";
 import { validators } from "./validators";
 
-export { validators, linter, adapters };
+export { validators, linter, adapters, parse };
 
 export * from "lib/config";
 export * from "lib/types";
