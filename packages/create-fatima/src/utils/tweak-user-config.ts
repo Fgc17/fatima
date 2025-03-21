fecd1a84630fc67cc4e49756eade2f1eadbbdff2
fgc17 <fgcoelho1709@outlook.com>
2025-03-21 03:16:06 -0300

    fix (create-fatima): patch error caused by lack of early return

--- packages/create-fatima/src/utils/tweak-user-config.ts
+++ packages/create-fatima/src/utils/tweak-user-config.ts
@@ -1,9 +1,4 @@
-import {
-	existsSync,
-	readFileSync,
-	appendFileSync,
-	writeFileSync,
-} from "node:fs";
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
 import path from "node:path";
 import type { AnyType } from "../lib/types";
 import { parse, stringify, assign } from "comment-json";
@@ -13,13 +8,15 @@ function getConfigPath(filename: string): string | null {
 
 	while (currentDir !== path.parse(currentDir).root) {
 		const configPath = path.join(currentDir, filename);
+
 		if (existsSync(configPath)) {
 			return configPath;
 		}
+
 		currentDir = path.dirname(currentDir);
 	}
 
-	return null;
+	return path.join(process.cwd(), filename);
 }
 
 export function tweakUserConfig(
@@ -27,42 +24,56 @@ export function tweakUserConfig(
 	modifier: (config: AnyType) => AnyType,
 ): AnyType {
 	const filePath = getConfigPath(fileName);
+
 	let updatedFile = "";
 
 	if (!filePath) return;
 
 	try {
-		if (fileName.endsWith(".json")) {
-			if (!existsSync(filePath)) {
-				writeFileSync(filePath, "{}");
+		const fileExtension = path.extname(fileName);
+
+		switch (fileExtension) {
+			case ".json": {
+				if (!existsSync(filePath)) {
+					writeFileSync(filePath, "{}");
+				}
+
+				const fileContent = readFileSync(filePath, "utf8");
+
+				const config = parse(fileContent, null, false);
+
+				const changed = assign(config, modifier(config));
+
+				const newContent = stringify(changed, null, 2);
+
+				updatedFile = newContent;
+
+				break;
 			}
+			default: {
+				if (!existsSync(filePath)) {
+					writeFileSync(filePath, "");
+				}
 
-			const fileContent = readFileSync(filePath, "utf8");
+				const currentContent = readFileSync(filePath, "utf8");
 
-			const config = parse(fileContent, null, false);
+				const newContent = modifier(currentContent);
 
-			const changed = assign(config, modifier(config));
+				if (!newContent) return;
 
-			const newContent = stringify(changed, null, 2);
+				updatedFile = newContent;
 
-			updatedFile = newContent;
+				break;
+			}
 		}
-
-		if (!existsSync(filePath)) {
-			writeFileSync(filePath, "");
-		}
-
-		const currentContent = readFileSync(filePath, "utf8");
-
-		const newContent = modifier(currentContent);
-
-		if (!newContent) return;
-
-		updatedFile = newContent;
 	} catch (e) {
 		console.error(`Error reading ${fileName}:`, e);
 		return;
 	}
+
+	console.log(
+		`Tweaked ${filePath} with the following changes:\n${updatedFile}`,
+	);
 
 	writeFileSync(filePath, updatedFile);
 }
