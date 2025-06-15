1b06182c67bacfb961fce422e139bad989606bd0
fgc17 <fgcoelho1709@outlook.com>
2025-06-14 23:47:19 -0300

    feat: add siteUrl option to infisical adapter

--- packages/fatima/src/core/adapters/infisical.ts
+++ packages/fatima/src/core/adapters/infisical.ts
@@ -1,52 +1,60 @@
 import type {
 	FatimaBuiltInLoadFunction,
-	FatimaLoadConfig,
 	UnsafeEnvironmentVariables,
 } from "lib/types";
 import type { GenericClass, AnyType } from "lib/utils/types";
 
 import { lifecycle } from "lib/lifecycle";
 
-type InfisicalClientMock = GenericClass<{
-	auth: () => {
-		universalAuth: {
-			login: (args: {
-				clientId: string;
-				clientSecret: string;
-			}) => Promise<AnyType>;
+type InfisicalClientMock = GenericClass<
+	{
+		auth: () => {
+			universalAuth: {
+				login: (args: {
+					clientId: string;
+					clientSecret: string;
+				}) => Promise<AnyType>;
+			};
 		};
-	};
-	secrets: () => {
-		listSecrets: (args: { environment: string; projectId: string }) => Promise<{
-			secrets: Array<{
-				secretKey: string;
-				secretValue: string;
+		secrets: () => {
+			listSecrets: (args: {
+				environment: string;
+				projectId: string;
+			}) => Promise<{
+				secrets: Array<{
+					secretKey: string;
+					secretValue: string;
+				}>;
 			}>;
-		}>;
-	};
-}>;
+		};
+	},
+	| [
+			{
+				siteUrl: string;
+			},
+	  ]
+	| [undefined]
+>;
 
 const load =
 	(
 		infisicalClient: InfisicalClientMock,
-		config?: FatimaLoadConfig<{
+		config?: {
 			clientId?: string;
 			clientSecret?: string;
 			projectId?: string;
 			environment?: string;
-		}>,
+			siteUrl?: string;
+		},
 	): FatimaBuiltInLoadFunction =>
 	async () => {
-		const client = new infisicalClient();
-
-		const configObject = config
-			? config(process.env as UnsafeEnvironmentVariables)
-			: {};
+		const configObject = config ?? {};
 
 		const auth = {
 			clientId: process.env.INFISICAL_CLIENT_ID,
 			clientSecret: process.env.INFISICAL_CLIENT_SECRET,
 			projectId: process.env.INFISICAL_PROJECT_ID,
+			siteUrl: process.env.INFISICAL_SITE_URL,
 			environment: "dev",
 			...configObject,
 		};
@@ -62,6 +70,9 @@ const load =
 		if (!auth.projectId) {
 			return lifecycle.error.missingConfig("INFISICAL_PROJECT_ID");
 		}
+
+		const siteUrl = auth.siteUrl;
+		const client = new infisicalClient(siteUrl ? { siteUrl } : undefined);
 
 		await client.auth().universalAuth.login({
 			clientId: auth.clientId,
