df2189276e0573c93c972f01fde2d63e55bc660f
fgc17 <fgcoelho1709@outlook.com>
2025-06-15 15:27:40 -0300

    refactor (fatima): add json safe parsing

--- packages/fatima/src/lib/tsconfig/tsconfig.ts
+++ packages/fatima/src/lib/tsconfig/tsconfig.ts
@@ -1,16 +1,20 @@
-import path from "node:path";
 import fs from "node:fs";
+import path from "node:path";
+import { jsonSafeParse } from "lib/utils/json";
 
 export const getTsconfigAliases = (): Record<string, string> => {
 	const tsconfigPath = path.resolve(process.cwd(), "tsconfig.json");
 
-	const tsconfig = JSON.parse(fs.readFileSync(tsconfigPath, "utf8"));
+	const { json, error } = jsonSafeParse(fs.readFileSync(tsconfigPath, "utf8"));
+
+	if (error) {
+		throw new Error("Failed to parse tsconfig.json", {
+			cause: error,
+		});
+	}
 
 	const basePath = path.dirname(tsconfigPath);
-	const paths = (tsconfig.compilerOptions?.paths ?? {}) as Record<
-		string,
-		string[]
-	>;
+	const paths = (json.compilerOptions?.paths ?? {}) as Record<string, string[]>;
 
 	const aliases: Record<string, string> = {};
 	for (const [alias, values] of Object.entries(paths)) {
